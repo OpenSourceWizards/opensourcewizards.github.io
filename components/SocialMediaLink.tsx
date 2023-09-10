11ce780b39f8e1ceeb4876d717f4a6b3fd49da7d
yankikucuk <kendinikertenkelebek@me.com>
2023-09-10 19:32:37 +0300

    update

--- components/SocialMediaLink.tsx
+++ components/SocialMediaLink.tsx
@@ -1,13 +1,17 @@
-import { IconBrandTwitter } from '@tabler/icons-react';
+import { IconBrandInstagram } from '@tabler/icons-react';
 
-const SocialMediaLink: React.FC<{ image: string; name: string; twitterName: string }> = ({ image, name, twitterName }) => {
+const SocialMediaLink: React.FC<{ image: string; name: string; instagramName: string }> = ({
+  image,
+  name,
+  instagramName,
+}) => {
   return (
     <div className="flex w-32 flex-col items-center justify-center gap-2">
       <p>{name}</p>
       <img src={image} alt={`${name}-image`} className="rounded-md bg-neutral-100 shadow-md dark:bg-neutral-900" />
-      <a href={`https://twitter.com/${twitterName}`} target="_blank" className="w-full">
+      <a href={`https://instagram.com/${instagramName}`} target="_blank" className="w-full">
         <button className="flex w-full items-center justify-center gap-2 rounded-md bg-sky-500/20 p-2 text-sky-500 hover:bg-sky-500/30">
-          <IconBrandTwitter />
+          <IconBrandInstagram />
           İletişim
         </button>
       </a>
